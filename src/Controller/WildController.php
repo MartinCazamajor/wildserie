6457af69cff563656eca4742b69b4fa03ced14d9
Martin Cazamajor <martin.cazamajor@gmail.com>
2019-11-26 11:53:18 +0100

    creation du premier formulaire du cours

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -6,7 +6,9 @@ use App\Entity\Category;
 use App\Entity\Episode;
 use App\Entity\Program;
 use App\Entity\Season;
+use App\Form\ProgramSearchType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -31,11 +33,17 @@ class WildController extends AbstractController
                 "No program found in program's table."
                 );
         }
+        $form = $this->createForm(
+            ProgramSearchType::class,
+            null,
+            ['method' => Request::METHOD_GET]
+        );
 
         return $this->render(
-            'wild/index.html.twig',
-            ['programs' => $programs]
-        );
+            'wild/index.html.twig', [
+                'programs' => $programs,
+                'form' => $form->createView(),
+                ]);
     }
 
     /**
